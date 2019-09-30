9558e7ad87e53a42ab7e99dd1872c133c1f274d5
Ryan Pavlik <ryan.pavlik@collabora.com>
2019-09-30 14:02:31 -0500

    Allow C++ ps/2 implementations

--- tmk_core/protocol/ps2_io.h
+++ tmk_core/protocol/ps2_io.h
@@ -1,6 +1,9 @@
 #ifndef PS2_IO_H
 #define PS2_IO_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void clock_init(void);
 void clock_lo(void);
@@ -11,5 +14,10 @@ void data_init(void);
 void data_lo(void);
 void data_hi(void);
 bool data_in(void);
+
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
 #endif
